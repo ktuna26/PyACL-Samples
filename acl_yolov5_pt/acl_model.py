8a801427eb91e7af4e4e4fcd641baa1c148f7158
Tianyu Zhou <tianyu.zhou@huawei.com>
2021-02-24 15:28:55 +0100

    added pytorch yolov5

---
+++ acl_yolov5_pt/acl_model.py
@@ -0,0 +1,282 @@
+"""
+Copyright (R) @huawei.com, all rights reserved
+-*- coding:utf-8 -*-
+CREATED:  2020-6-04 20:12:13
+MODIFIED: 2020-6-28 14:04:45
+"""
+import acl
+import struct
+import numpy as np
+import torch
+import torchvision
+from constant import ACL_MEM_MALLOC_NORMAL_ONLY, \
+    ACL_MEMCPY_HOST_TO_DEVICE, ACL_MEMCPY_DEVICE_TO_HOST, \
+    ACL_ERROR_NONE, NPY_BYTE
+from acl_util import check_ret
+import cv2
+from postprocessing import get_model_output_by_index, letterbox, focus_process, resize_image, detect, non_max_suppression, scale_coords
+
+class Model(object):
+    def __init__(self,
+                 device_id,
+                 model_path,
+                 model_input_width,
+                 model_input_height):
+        self.device_id = device_id
+        self.model_path = model_path    # string
+        self.model_id = None            # pointer
+        self.context = None             # pointer
+        self.stream = None
+        self.input_data = None
+        self.output_data = None
+        self.model_desc = None          # pointer when using
+        self.input0_dataset_buffer = None
+        self.input1_dataset_buffer = None
+        self.input1_buffer = None
+        self.model_input_width = model_input_width
+        self.model_input_height = model_input_height
+        self.input_dataset = None
+        self.init_resource()
+        
+
+    def __del__(self):
+        self._release_dataset()
+        if self.model_id:
+            ret = acl.mdl.unload(self.model_id)
+            check_ret("acl.mdl.unload", ret)
+
+        if self.model_desc:
+            ret = acl.mdl.destroy_desc(self.model_desc)
+            check_ret("acl.mdl.destroy_desc", ret)
+
+        if self.input1_buffer:
+            ret = acl.rt.free(self.input1_buffer)
+            check_ret("acl.rt.free", ret)
+
+        print("[Model] class Model release source success")
+        
+        if self.stream:
+            acl.rt.destroy_stream(self.stream)
+
+        if self.context:
+            acl.rt.destroy_context(self.context)
+        acl.rt.reset_device(self.device_id)
+        acl.finalize()
+        
+        print("[ACL] class Sample release source success")
+
+    def init_resource(self):
+        print("[ACL] init resource stage:")
+        acl.init()
+        ret = acl.rt.set_device(self.device_id)
+        check_ret("acl.rt.set_device", ret)
+
+        self.context, ret = acl.rt.create_context(self.device_id)
+        check_ret("acl.rt.create_context", ret)
+
+        self.stream, ret = acl.rt.create_stream()
+        check_ret("acl.rt.create_stream", ret)
+        print("[ACL] init resource stage success")
+        
+        print("[Model] class Model init resource stage:")
+        # context
+        acl.rt.set_context(self.context)
+        # load_model
+        self.model_id, ret = acl.mdl.load_from_file(self.model_path)
+        check_ret("acl.mdl.load_from_file", ret)
+        self.model_desc = acl.mdl.create_desc()
+        ret = acl.mdl.get_desc(self.model_desc, self.model_id)
+        check_ret("acl.mdl.get_desc", ret)
+        
+        input_size = acl.mdl.get_num_inputs(self.model_desc)
+        output_size = acl.mdl.get_num_outputs(self.model_desc)
+        self._gen_output_dataset(output_size)
+        print("model input size", input_size)
+        for i in range(input_size):
+            print("input ", i)
+            print("model input dims", acl.mdl.get_input_dims(self.model_desc, i))
+            print("model input datatype", acl.mdl.get_input_data_type(self.model_desc, i))
+        print("=" * 50)
+        print("model output size", output_size)
+        for i in range(output_size):
+            print("output ", i)
+            print("model output dims", acl.mdl.get_output_dims(self.model_desc, i))
+            print("model output datatype", acl.mdl.get_output_data_type(self.model_desc, i))
+        print("=" * 50)
+        print("[Model] class Model init resource stage success")
+   
+    def transfer_img_to_device(self, img_resized):
+        
+        # BGR to RGB
+        img_host_ptr = acl.util.numpy_to_ptr(img_resized)
+        print(img_host_ptr)
+        img_buf_size = img_resized.itemsize * img_resized.size
+        print("img_buf_size", img_buf_size)
+        img_dev_ptr, ret = acl.rt.malloc(img_buf_size, ACL_MEM_MALLOC_NORMAL_ONLY)
+        check_ret("acl.rt.malloc", ret)
+        ret = acl.rt.memcpy(img_dev_ptr, img_buf_size, img_host_ptr, img_buf_size, ACL_MEMCPY_HOST_TO_DEVICE)
+        check_ret("acl.rt.memcpy", ret)
+        
+        return img_dev_ptr, img_buf_size
+    
+    def run1(self, img):
+        self.img = resize_image(img, (self.model_input_width, self.model_input_height))
+        self.img = self.img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x640x640
+        image_np = np.array(self.img, dtype=np.float32)
+        image_np /= 255.0
+        image_np_expanded = np.expand_dims(image_np, axis=0)  # NCHW
+        # Focus
+        image_np_expanded = focus_process(image_np_expanded)
+        print("image_np_expanded shape:", image_np_expanded.shape)
+        img_numpy = np.ascontiguousarray(image_np_expanded)
+        print("img_numpy shape:", img_numpy.shape)
+        
+        img_dev_ptr, img_buf_size = self.transfer_img_to_device(img_numpy)
+#         print("img_dev_ptr, img_buf_size: ", img_dev_ptr, img_buf_size)
+        self._gen_input_dataset(img_dev_ptr, img_buf_size)
+        self.forward()
+        
+        ret = acl.rt.free(img_dev_ptr)
+        check_ret("acl.rt.free", ret)
+        
+        
+        pred_sbbox = get_model_output_by_index(self.output_data, 0)
+        pred_mbbox = get_model_output_by_index(self.output_data, 1)
+        pred_lbbox = get_model_output_by_index(self.output_data, 2)
+        feature_maps = [pred_sbbox, pred_mbbox, pred_lbbox]
+        
+        yolo_shape = [[1, 3, 85, 80, 80], [1, 3, 85, 40, 40], [1, 3, 85, 20, 20]]
+        
+        for idx, feat, tgt_shape in zip(range(3), feature_maps, yolo_shape):
+            feature_maps[idx] = feat.reshape(tgt_shape).transpose((0, 1, 3, 4, 2))
+            
+        res_tensor = detect(feature_maps)
+        res_tensor = torch.from_numpy(res_tensor)
+
+        # Apply NMS
+        pred = non_max_suppression(res_tensor, conf_thres=0.33, iou_thres=0.5, classes=None, agnostic=False)
+        
+        # Process detections
+        bboxes = []
+        src_img = img
+        
+        for i, det in enumerate(pred):  # detections per image
+            # Rescale boxes from img_size to im0 size
+            if det is not None:
+                det[:, :4] = scale_coords((self.model_input_width, self.model_input_height), det[:, :4], src_img.shape).round()
+                for *xyxy, conf, cls in det:
+                    bboxes.append([*xyxy, conf, int(cls)])
+            else:
+                pass
+        return bboxes
+    
+    def run(self, img):
+        
+        img_resized = resize_image(img, (self.model_input_width, self.model_input_height))[:, :, ::-1]
+        img_resized = (img_resized/255).astype(np.float32)
+        img_resized = img_resized.transpose(2, 0, 1)
+        
+        img_resized = np.expand_dims(img_resized, axis=0)
+        print("----", img_resized.shape)
+        
+        img_focused = focus_process(img_resized)
+        print("----", img_focused.shape)
+        
+        img_dev_ptr, img_buf_size = self.transfer_img_to_device(img_focused)
+#         print("img_dev_ptr, img_buf_size: ", img_dev_ptr, img_buf_size)
+        self._gen_input_dataset(img_dev_ptr, img_buf_size)
+        self.forward()
+        
+        ret = acl.rt.free(img_dev_ptr)
+        check_ret("acl.rt.free", ret)
+        
+        
+        pred_sbbox = get_model_output_by_index(self.output_data, 0)
+        pred_mbbox = get_model_output_by_index(self.output_data, 1)
+        pred_lbbox = get_model_output_by_index(self.output_data, 2)
+        
+        
+        
+        return [pred_sbbox, pred_mbbox, pred_lbbox]
+#         return []
+        self.pred_bbox = np.concatenate([pred_sbbox, \
+                                    pred_mbbox, \
+                                    pred_lbbox], axis=-1)
+        
+        print("pred_bbox shape", self.pred_bbox.shape)
+        original_image_size = img.shape[:2]
+        print("original_image_size", original_image_size)
+#         bboxes = postprocess_boxes(self.pred_bbox, original_image_size, self.model_input_width, 0.3)
+#         print(bboxes)
+#         bboxes = nms(bboxes, 0.3, method='nms')
+#         print(bboxes)
+        return []
+        return bboxes
+
+    def forward(self):
+        print('[Model] execute stage:')
+        ret = acl.mdl.execute(self.model_id,
+                              self.input_dataset,
+                              self.output_data)
+        check_ret("acl.mdl.execute", ret)
+
+        #free the input dataset
+        if self.input0_dataset_buffer:
+            ret = acl.destroy_data_buffer(self.input0_dataset_buffer)
+            check_ret("acl.destroy_data_buffer", ret)
+            self.input0_dataset_buffer = None
+        if self.input_dataset:
+            ret = acl.mdl.destroy_dataset(self.input_dataset)
+            check_ret("acl.destroy_dataset", ret)
+            self.input_dataset = None
+
+        print('[Model] execute stage success')
+
+    def _gen_input_dataset(self, dvpp_output_buffer, dvpp_output_size, image_height=None, image_width=None):
+        print("[Model] create model input dataset:")
+        self.input_dataset = acl.mdl.create_dataset()
+        self.input0_dataset_buffer = acl.create_data_buffer(dvpp_output_buffer,
+                                                      dvpp_output_size)
+        _, ret = acl.mdl.add_dataset_buffer(
+            self.input_dataset,
+            self.input0_dataset_buffer)
+        if ret:
+            ret = acl.destroy_data_buffer(self.input0_dataset_buffer)
+            self.input0_dataset_buffer = None
+            check_ret("acl.destroy_data_buffer", ret)
+
+        print("[Model] create model input dataset success")
+
+    def _gen_output_dataset(self, size):
+        print("[Model] create model output dataset:")
+        dataset = acl.mdl.create_dataset()
+        for i in range(size):
+            temp_buffer_size = acl.mdl.\
+                get_output_size_by_index(self.model_desc, i)
+            temp_buffer, ret = acl.rt.malloc(temp_buffer_size,
+                                             ACL_MEM_MALLOC_NORMAL_ONLY)
+            check_ret("acl.rt.malloc", ret)
+            dataset_buffer = acl.create_data_buffer(
+                temp_buffer,
+                temp_buffer_size)
+
+            _, ret = acl.mdl.add_dataset_buffer(dataset, dataset_buffer)
+            if ret:
+                acl.destroy_data_buffer(dataset)
+                check_ret("acl.destroy_data_buffer", ret)
+        self.output_data = dataset
+        print("[Model] create model output dataset success")
+        
+    def _release_dataset(self, ):
+        for dataset in [self.input_dataset, self.output_data]:
+            if not dataset:
+                continue
+            num = acl.mdl.get_dataset_num_buffers(dataset)
+            for i in range(num):
+                data_buf = acl.mdl.get_dataset_buffer(dataset, i)
+                if data_buf:
+                    ret = acl.destroy_data_buffer(data_buf)
+                    check_ret("acl.destroy_data_buffer", ret)
+
+            ret = acl.mdl.destroy_dataset(dataset)
+            check_ret("acl.mdl.destroy_dataset", ret)
---
+++ acl_yolov5_pt/postprocessing.py
@@ -0,0 +1,232 @@
+import numpy as np
+import acl
+import cv2
+import torch
+import torchvision
+import time
+from constant import ACL_MEM_MALLOC_NORMAL_ONLY, \
+    ACL_MEMCPY_HOST_TO_DEVICE, ACL_MEMCPY_DEVICE_TO_HOST, \
+    ACL_ERROR_NONE, NPY_BYTE
+
+def resize_image(img, size):
+
+    h, w = img.shape[:2]
+    c = img.shape[2] if len(img.shape)>2 else 1
+
+    if h == w: 
+        return cv2.resize(img, size, cv2.INTER_AREA)
+
+    dif = h if h > w else w
+
+    interpolation = cv2.INTER_AREA if dif > (size[0]+size[1])//2 else \
+                    cv2.INTER_CUBIC
+
+    x_pos = (dif - w)//2
+    y_pos = (dif - h)//2
+
+    if len(img.shape) == 2:
+        mask = np.zeros((dif, dif), dtype=img.dtype)
+        mask.fill(128)
+        mask[y_pos:y_pos+h, x_pos:x_pos+w] = img[:h, :w]
+    else:
+        mask = np.zeros((dif, dif, c), dtype=img.dtype)
+        mask.fill(128)
+        mask[y_pos:y_pos+h, x_pos:x_pos+w, :] = img[:h, :w, :]
+
+    return cv2.resize(mask, size, interpolation)
+
+def letterbox(img, new_shape=(640, 640), color=(128, 128, 128), auto=True, scaleFill=False, scaleup=True):
+    # Resize image to a 32-pixel-multiple rectangle https://github.com/ultralytics/yolov3/issues/232
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+def focus_process(x):
+    # x(b,c,w,h) -> y(b,4c,w/2,h/2)
+    return np.concatenate([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
+    
+
+def get_model_output_by_index(model_output, i):
+    temp_output_buf = acl.mdl.get_dataset_buffer(model_output, i)
+
+    infer_output_ptr = acl.get_data_buffer_addr(temp_output_buf)
+    infer_output_size = acl.get_data_buffer_size(temp_output_buf)
+
+    output_host, _ = acl.rt.malloc_host(infer_output_size)
+    acl.rt.memcpy(output_host, infer_output_size, infer_output_ptr,
+                          infer_output_size, ACL_MEMCPY_DEVICE_TO_HOST)
+
+    return acl.util.ptr_to_numpy(output_host, (infer_output_size//4,), 11)
+
+def _make_grid(nx=20, ny=20):
+    xv, yv = np.meshgrid(np.arange(nx), np.arange(ny))
+    return np.stack((xv, yv), 2).reshape((1, 1, ny, nx, 2)).astype(np.float)
+
+
+def sigmoid(x0):
+    s = 1 / (1 + np.exp(-x0))
+    return s
+
+def detect(x, model_type="yolov5"):
+    """
+    x(bs,3,20,20,85)
+    """
+
+    # x(bs,3,20,20,85)
+    z = []
+    grid = []
+    for i in range(3):
+        _, _, ny, nx, _ =  x[i].shape
+        grid.append(_make_grid(nx, ny))
+    if model_type == 'yolov5':
+        stride =  np.array([8, 16, 32])
+        anchor_grid = np.array(
+            [[10., 13, 16, 30, 33, 23], [30, 61, 62, 45, 59, 119], [116, 90, 156, 198, 373, 326]])\
+            .reshape(3, 1, 3, 1, 1, 2)
+    elif model_type == 'yolov3':
+        stride = np.array([32, 16, 8])
+        anchor_grid = np.array(
+            [[116, 90, 156, 198, 373, 326], [30, 61, 62, 45, 59, 119], [10., 13, 16, 30, 33, 23]])\
+            .reshape(3, 1, 3, 1, 1, 2)
+
+    for i in range(3):
+        y = sigmoid(x[i])
+        y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + grid[i]) * stride[i]  # xy
+        y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * anchor_grid[i]  # wh
+        z.append(y.reshape(1, -1, 85))
+    return np.concatenate(z, 1)
+
+
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+
+def non_max_suppression(prediction, conf_thres=0.1, iou_thres=0.6, merge=False, classes=None, agnostic=False):
+    """Performs Non-Maximum Suppression (NMS) on inference results
+
+    Returns:
+         detections with shape: nx6 (x1, y1, x2, y2, conf, cls)
+    """
+
+    nc = prediction[0].shape[1] - 5  # number of classes
+    xc = prediction[..., 4] > conf_thres  # candidates
+
+    # Settings
+    min_wh, max_wh = 2, 4096  # (pixels) minimum and maximum box width and height
+    max_det = 300  # maximum number of detections per image
+    time_limit = 10.0  # seconds to quit after
+    redundant = True  # require redundant detections
+    multi_label = nc > 1  # multiple labels per box (adds 0.5ms/img)
+
+    t = time.time()
+    output = [None] * prediction.shape[0]
+    for xi, x in enumerate(prediction):  # image index, image inference
+        # Apply constraints
+        # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
+        x = x[xc[xi]]  # confidence
+
+        # If none remain process next image
+        if not x.shape[0]:
+            continue
+
+        # Compute conf
+        x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+
+        # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+        box = xywh2xyxy(x[:, :4])
+
+        # Detections matrix nx6 (xyxy, conf, cls)
+        if multi_label:
+            i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+            x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
+        else:  # best class only
+            conf, j = x[:, 5:].max(1, keepdim=True)
+            x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
+
+        # Filter by class
+        if classes:
+            x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+
+        # Apply finite constraint
+        # if not torch.isfinite(x).all():
+        #     x = x[torch.isfinite(x).all(1)]
+
+        # If none remain process next image
+        n = x.shape[0]  # number of boxes
+        if not n:
+            continue
+        # Sort by confidence
+        # x = x[x[:, 4].argsort(descending=True)]
+
+        # Batched NMS
+        c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+        boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
+        i = torchvision.ops.nms(boxes, scores, iou_thres)
+        if i.shape[0] > max_det:  # limit detections
+            i = i[:max_det]
+        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+            try:  # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+                iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+                weights = iou * scores[None]  # box weights
+                x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
+                if redundant:
+                    i = i[iou.sum(1) > 1]  # require redundancy
+            except:  # possible CUDA error https://github.com/ultralytics/yolov3/issues/1139
+                print(x, i, x.shape, i.shape)
+                pass
+
+        output[xi] = x[i]
+        if (time.time() - t) > time_limit:
+            break  # time limit exceeded
+
+    return output
+
+
+def clip_coords(boxes, img_shape):
+    # Clip bounding xyxy bounding boxes to image shape (height, width)
+    boxes[:, 0].clamp_(0, img_shape[1])  # x1
+    boxes[:, 1].clamp_(0, img_shape[0])  # y1
+    boxes[:, 2].clamp_(0, img_shape[1])  # x2
+    boxes[:, 3].clamp_(0, img_shape[0])  # y2
+
+
+def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
+    # Rescale coords (xyxy) from img1_shape to img0_shape
+    if ratio_pad is None:  # calculate from img0_shape
+        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
+
+    coords[:, [0, 2]] -= pad[0]  # x padding
+    coords[:, [1, 3]] -= pad[1]  # y padding
+    coords[:, :4] /= gain
+    clip_coords(coords, img0_shape)
+    return coords
