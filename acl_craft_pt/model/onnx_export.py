fc9088bbae0ef3752feb77ab26c280656df890ea
Kubilay Tuna <kubilay.tuna@huawei.com>
2021-11-16 10:35:06 +0000

    update acl_craft_pt/model/onnx_export.py.

--- acl_craft_pt/model/onnx_export.py
+++ acl_craft_pt/model/onnx_export.py
@@ -1,7 +1,4 @@
 import torch
-from torch.autograd import Variable
-import cv2
-import imgproc
 from craft import CRAFT
 from collections import OrderedDict
 
@@ -18,27 +15,15 @@ def copyStateDict(state_dict):
     return new_state_dict
 
 # load net
-net = CRAFT()     # initialize
-#net = net.cuda()
-#net = torch.nn.DataParallel(net)
+net = CRAFT() # initialize
 
 net.load_state_dict(copyStateDict(torch.load('./weights/craft_mlt_25k.pth', map_location='cpu')))
 net.eval()
 
 # load data
-image = imgproc.loadImage('./template/test.jpg')
-
-# resize
-img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(image, 1280, interpolation=cv2.INTER_LINEAR, mag_ratio=1.5)
-ratio_h = ratio_w = 1 / target_ratio
-
-# preprocessing
-x = imgproc.normalizeMeanVariance(img_resized)
-x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
-x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
-#x = x.cuda()
+img = torch.zeros((1, 3, 736, 1280)) # BCHW
 
 from collections import OrderedDict
 
 # trace export
-torch.onnx.export(net, x, 'craft.onnx', export_params=True, verbose=True, opset_version=11)
+torch.onnx.export(net, img, 'craft.onnx', export_params=True, verbose=True, opset_version=11)
